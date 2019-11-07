f0487bee78a0a0885aad5da7a5b79ab556430a6e
granatb <32493624+granatb@users.noreply.github.com>
2019-11-07 01:49:34 +0100

    sleep mode: activated
    mozna dodawac jedno logo dwa lub 0

--- tests/initial_tests.R
+++ tests/initial_tests.R
@@ -2,9 +2,11 @@ library(miniBeamer)
 library(rmarkdown)
 ### While having working directory at project level
 rmarkdown::render('tests/initial_test/example.Rmd', miniBeamer::beam_this_rmd(toc = TRUE,
-                                                                              latex_engine = 'pdflatex',
-                                                                              themecolor = 'graphite',
-                                                                              fontcolor = 'heather'))
+                                                                              latex_engine = 'xelatex',
+                                                                              themecolor = 'white',
+                                                                              fontcolor = 'black',
+                                                                              bl = "C:/Users/bgranat/Desktop/logoPW.png",
+                                                                              br = "C:/Users/bgranat/Desktop/WMINIznak.png"))
 
 
 
