611f4bd33e7da5c7ad86f8999f28606f712bb6ba
Adam Rydelek <32574203+Adamoso@users.noreply.github.com>
2019-11-12 19:45:57 +0100

    Updating tests

--- tests/run_commands.R
+++ tests/run_commands.R
@@ -3,14 +3,29 @@ library(rmarkdown)
 
 ### .Rmd to .pdf
 rmarkdown::render('tests/prezentacja/prezentacja_pakietu.Rmd', miniBeamer::beam_this_rmd(toc = TRUE,
-                                                                              latex_engine = 'xelatex',
-                                                                              themecolor = 'white',
-                                                                              fontcolor = 'black',
-                                                                              bl = "C:/Users/bgranat/Desktop/logoPW.png",
-                                                                              br = "C:/Users/bgranat/Desktop/WMINIznak.png"))
+                                                                                         latex_engine = 'pdflatex',
+                                                                                         themecolor = 'white',
+                                                                                         fontcolor = 'black',
+                                                                                         bl = "C:/Users/Adam/Desktop/logoPW.jpg",
+                                                                                         br = "C:/Users/Adam/Desktop/logoMini.png"))
 
-### .Rnw to .rmd
-miniBeamer::rnw_to_rmd('tests/rnw_to_rmd/rnwExample.Rnw')
+### .Rnw to .Rmd
+## In order for the conversion to work properly the working directory
+## has to be in the folder containing the .Rnw file.
+oldWd <- getwd()
+newWd <- paste(getwd(), "/tests/rnw_to_rmd", sep="")
+setwd(newWd)
+
+miniBeamer::rnw_to_rmd('rnwExample.Rnw')
+rmarkdown::render('rnwExample.Rmd', miniBeamer::beam_this_rmd(toc = TRUE,
+                                                                        latex_engine = 'pdflatex',
+                                                                        themecolor = 'sapphire',
+                                                                        fontcolor = 'graphite',
+                                                                        bl = "C:/Users/Adam/Desktop/logoPW.jpg",
+                                                                        br = "C:/Users/Adam/Desktop/logoMini.png"))
+
+setwd(oldWd)
 
 ### Business card generator
 rmarkdown::render('tests/business_card/business_card.Rmd', miniBeamer::business_card())
+
