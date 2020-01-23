29b1e4f0b0d2ecf74615d7d6fb1a997cf5dc853a
mckraqs <mckraqs@gmail.com>
2020-01-23 13:01:20 +0100

    Adding tests

--- tests/testthat/test_rmd_to_beamer.R
+++ tests/testthat/test_rmd_to_beamer.R
@@ -2,13 +2,10 @@ context("Testing rmd_to_beamer function")
 
 test_that("Testing equivalency of generated files",{
 
+  rmarkdown::render('./../test_items/rmd_to_beamer_test_base.rmd', miniBeamer::rmd_to_beamer(toc = TRUE, latex_engine = 'pdflatex'))
   rmarkdown::render('./../test_items/rmd_to_beamer_test.rmd', miniBeamer::rmd_to_beamer(toc = TRUE, latex_engine = 'pdflatex'))
   rendered <- readLines("./../test_items/rmd_to_beamer_test.tex")
   base <- readLines("./../test_items/rmd_to_beamer_test_base.tex")
-
-  cat(rendered)
-
-  cat(base)
 
   rend_1 <- rendered[15]
   check_1 <- rend_1 == base[15]
