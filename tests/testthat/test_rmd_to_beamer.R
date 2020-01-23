0e54a55942f233284e6bb87e1b3e2569d93e57f8
mckraqs <mckraqs@gmail.com>
2020-01-23 12:57:01 +0100

    Adding tests

--- tests/testthat/test_rmd_to_beamer.R
+++ tests/testthat/test_rmd_to_beamer.R
@@ -6,9 +6,21 @@ test_that("Testing equivalency of generated files",{
   rendered <- readLines("./../test_items/rmd_to_beamer_test.tex")
   base <- readLines("./../test_items/rmd_to_beamer_test_base.tex")
 
+  cat(rendered)
+
+  cat(base)
+
   rend_1 <- rendered[15]
   check_1 <- rend_1 == base[15]
   expect_true(check_1)
+
+  # rend_2 <- rendered[45]
+  # check_2 <- rend_2 == base[45]
+  # expect_true(check_2)
+  #
+  # rend_3 <- rendered[70]
+  # check_3 <- rend_3 == base[70]
+  # expect_true(check_3)
 
   expect_error(rmd_to_beamer(), NA)
 })
