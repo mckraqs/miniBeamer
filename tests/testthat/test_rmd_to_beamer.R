9881979d4e2c9b04cb29343b1afe354c53e0062e
mckraqs <mckraqs@gmail.com>
2020-01-23 13:08:33 +0100

    Final version test 1

--- tests/testthat/test_rmd_to_beamer.R
+++ tests/testthat/test_rmd_to_beamer.R
@@ -10,13 +10,13 @@ test_that("Testing equivalency of generated files",{
   check_1 <- rend_1 == base[15]
   expect_true(check_1)
 
-  # rend_2 <- rendered[45]
-  # check_2 <- rend_2 == base[45]
-  # expect_true(check_2)
-  #
-  # rend_3 <- rendered[70]
-  # check_3 <- rend_3 == base[70]
-  # expect_true(check_3)
+  rend_2 <- rendered[45]
+  check_2 <- rend_2 == base[45]
+  expect_true(check_2)
+
+  rend_3 <- rendered[70]
+  check_3 <- rend_3 == base[70]
+  expect_true(check_3)
 
   expect_error(rmd_to_beamer(), NA)
 })
